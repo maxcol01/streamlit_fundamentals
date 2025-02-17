ce38d6e70439754a6ddd87676447e8aa32c10775
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2025-02-17 18:53:40 +0100

    adding helpers functions

--- project.py
+++ project.py
@@ -5,6 +5,13 @@ from langchain_community.document_loaders import (PyPDFLoader,
 												  Docx2txtLoader, 
 												  TextLoader)
 import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv(), override=True)
+
+# Loading API key
+API_OPENAI = os.getenv("API_OPENAI")
+
 
 # Loading the document into a Langchain format
 def load_document(file):
@@ -24,9 +31,32 @@ def load_document(file):
     data = loader.load()
     return data
 
+
 # Chunking the loaded document
-def chunk_data(data, chunk_size=256):
+def chunk_data(data, chunk_size=256, chunk_overlap=20):
 	from langchain.text_splitter import RecursiveCharacterTextSplitter
-	text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=0)
+	text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
 	chunks = text_splitter.split_documents(data)
 	return chunks
+
+
+# Create embeddings and vector store
+def create_embeddings(chunks):
+	embeddings = OpenAIEmbeddings(api_key=API_OPENAI)
+	vector_store = Chroma.from_documents(chunks, embeddings)
+	return vector_store
+
+
+# Create a Q/A function
+def ask_and_get_answer(vector_store, q, k=3):
+	from langchain.chains import RetrievalQA
+	from langchain.chat_models import ChatOpenAI
+	
+	llm = ChatOpenAI(api_key=API_OPENAI)
+	retriever = vector_store.as_retriever(search_type="similiraty", 
+                                          search_kwargs={"k":k})
+	chain = RetrievalQA.from_chain_type(llm=llm, 
+                                        chain_type="stuff", 
+                                        retriever=retriever)
+	answer = chain.invoke(q)
+	return answer
